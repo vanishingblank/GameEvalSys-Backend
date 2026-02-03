4de8a6e211dc761c2f0cb49e782632ac3ebf9b24
VBremix <15491509+vanishblank@user.noreply.gitee.com>
2026-02-03 22:00:43 +0800

    feat:编辑&删除用户Mapper

--- src/main/java/com/eval/gameeval/mapper/UserMapper.java
+++ src/main/java/com/eval/gameeval/mapper/UserMapper.java
@@ -2,11 +2,10 @@ package com.eval.gameeval.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.eval.gameeval.models.entity.User;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 
 @Mapper
 @Repository
@@ -28,4 +27,27 @@ public interface UserMapper extends BaseMapper<User> {
     @Insert("INSERT INTO sys_user(username, password, name, role, is_enabled, create_time, update_time) " +
             "VALUES(#{username}, #{password}, #{name}, #{role}, #{isEnabled}, #{createTime}, #{updateTime})")
     int insertUser(User user);
+
+
+    @Select("SELECT * FROM sys_user WHERE id = #{id}")
+    User selectById(@Param("id") Long id);
+
+
+
+    @Delete("DELETE FROM sys_user WHERE id = #{id}")
+    int deleteById(@Param("id") Long id);
+
+    @Update("UPDATE sys_user " +
+            "SET name = #{name}, " +
+            "    role = #{role}, " +
+            "    is_enabled = #{isEnabled}, " +
+            "    update_time = #{updateTime} " +
+            "WHERE id = #{id}")
+    int updateById(User user);
+
+
+    @Update("UPDATE sys_user SET is_enabled = 0, update_time = #{updateTime} WHERE id = #{id}")
+    int disableById(@Param("id") Long id, @Param("updateTime") LocalDateTime updateTime);
+
+
 }
