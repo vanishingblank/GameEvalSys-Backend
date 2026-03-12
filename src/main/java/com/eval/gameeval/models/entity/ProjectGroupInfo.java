c5a6cfb94c6d9de2548771694b4b4d92fe38aa08
LJR <1449466458@qq.com>
2026-03-12 21:47:00 +0800

    feat:project_group表分离之后entity调整

---
+++ src/main/java/com/eval/gameeval/models/entity/ProjectGroupInfo.java
@@ -0,0 +1,49 @@
+package com.eval.gameeval.models.entity;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * 小组信息主表实体类
+ * 存储小组的基本信息，与项目无关。可以被多个项目使用
+ */
+@Data
+@Accessors(chain = true)
+public class ProjectGroupInfo implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 小组ID
+     */
+    private Long id;
+
+    /**
+     * 小组名称
+     */
+    private String name;
+
+    /**
+     * 小组描述
+     */
+    private String description;
+
+    /**
+     * 是否启用：1-是 0-否
+     */
+    private Integer isEnabled;
+
+    /**
+     * 创建时间
+     */
+    private LocalDateTime createTime;
+
+    /**
+     * 更新时间
+     */
+    private LocalDateTime updateTime;
+}
